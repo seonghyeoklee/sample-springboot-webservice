f290b2070ad833ea5ce3df66a6fc586a5266657f
megamax23 <megamax23@gmail.com>
2020-03-06 18:32:26 +0900

    posts add

---
+++ src/main/java/com/shlee/springboot/domain/posts/Posts.java
@@ -0,0 +1,35 @@
+package com.shlee.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+}

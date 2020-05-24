e5a3ca4187e14895362f621e88045f9eb97f82e2
Seong Hyeok Lee <dltjdgur327@gmail.com>
2020-05-25 08:46:22 +0900

    OAuth google

--- src/main/java/com/shlee/book/springboot/domain/user/User.java
+++ src/main/java/com/shlee/book/springboot/domain/user/User.java
@@ -1,11 +1,16 @@
 package com.shlee.book.springboot.domain.user;
 
+import com.shlee.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Getter
-public class User {
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +27,23 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 }
